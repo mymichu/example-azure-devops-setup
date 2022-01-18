71dc7dfbca8677883135bc7ff0c79754669afb85
Michel Meyer <meyer_michel@outlook.com>
2022-01-18 05:43:24 +0100

    format python

--- tasks/azdevops.py
+++ tasks/azdevops.py
@@ -7,44 +7,50 @@ from tomlkit import string
 
 
 current_file_dir = pathlib.Path(__file__).parent.absolute()
-home_dir=f'{current_file_dir}/..'
+home_dir = f'{current_file_dir}/..'
 azuredevops_storage_ressource_group = "azure_devops_rg"
 azuredevops_storage_account_name = "azuredevopstest"
 azuredevops_storage_state = "state"
 
+
 @task
 def login(c):
-    az_user = os.getenv("AZ_USER",None)
-    az_password = os.getenv("AZ_PASSWORD",None)
+    az_user = os.getenv("AZ_USER", None)
+    az_password = os.getenv("AZ_PASSWORD", None)
     result = c.run("az account show -o none", hide=True, warn=True)
-    if az_user!=None and az_password!=None :
+    if az_user != None and az_password != None:
         c.run(f"az login -u {az_user} -p {az_password}")
-    elif result.ok==False:
+    elif result.ok == False:
         c.run("az login")
     print("You are logged in!")
 
-    
+
 @task(pre=[login])
 def setup_storage(c):
     location = "westeurope"
-    c.run(f"az group create --location {location} --resource-group {azuredevops_storage_ressource_group}")
-    c.run(f"az storage account create -n {azuredevops_storage_account_name} -g {azuredevops_storage_ressource_group} -l {location} --sku Standard_LRS")
-    c.run(f"az storage container create -n {azuredevops_storage_state} --account-name {azuredevops_storage_account_name}")
-    
+    c.run(
+        f"az group create --location {location} --resource-group {azuredevops_storage_ressource_group}")
+    c.run(
+        f"az storage account create -n {azuredevops_storage_account_name} -g {azuredevops_storage_ressource_group} -l {location} --sku Standard_LRS")
+    c.run(
+        f"az storage container create -n {azuredevops_storage_state} --account-name {azuredevops_storage_account_name}")
+
 
 def read_azdevops_environment():
     az_devops_token = os.getenv("AZ_DEVOPS_AGENT_TOKEN", None)
     az_devops_url = os.getenv("AZ_DEVOPS_AGENT_URL", None)
     if az_devops_token == None or az_devops_url == None:
-        sys.exit("ERROR: AZ_DEVOPS_AGENT_TOKEN and AZ_DEVOPS_AGENT_URL need to be set")
+        sys.exit(
+            "ERROR: AZ_DEVOPS_AGENT_TOKEN and AZ_DEVOPS_AGENT_URL need to be set")
     os.environ["TF_VAR_az_devops_token"] = az_devops_token
     os.environ["TF_VAR_az_devops_url"] = az_devops_url
-    
+
+
 @task(pre=[login])
 def init(c):
-    environment = os.getenv("ENVIRONMENT","test")
+    environment = os.getenv("ENVIRONMENT", "test")
     read_azdevops_environment()
-    
+
     os.chdir(home_dir)
     c.run(f'''rm -rf .terraform/modules .terraform/terraform.tfstate tf.plan
               terraform init -input=false \
@@ -53,17 +59,19 @@ def init(c):
               -backend-config="container_name={azuredevops_storage_state}" \
               -backend-config="key=environment/{environment}-azure-setup.tfstate" \
               -force-copy''')
-    
-    
+
+
 @task(pre=[init])
 def plan(c):
     os.chdir(home_dir)
     c.run('terraform plan -out=tf.plan')
-    
+
+
 @task(pre=[plan])
 def setup(c):
     os.chdir(home_dir)
     c.run('terraform apply tf.plan')
+
 
 @task
 def destroy(c):
--- tasks/docker.py
+++ tasks/docker.py
@@ -3,18 +3,21 @@ from invoke import task
 import pathlib
 
 
-
 current_file_dir = pathlib.Path(__file__).parent.absolute()
 dev_image = "devops_setup:latest"
 dev_container = "devops_setup"
 
+
 @task
 def setup(c):
     c.run(f"docker build {current_file_dir}/../.devcontainer -t {dev_image}")
-    
+
+
 @task(pre=setup)
 def run(c, image=dev_image):
-    c.run(f'nohup docker run --rm -v {current_file_dir}/..:/dev --name devops_setup -t {image}')
+    c.run(
+        f'nohup docker run --rm -v {current_file_dir}/..:/dev --name devops_setup -t {image}')
+
 
 @task
 def clean(c, image="iot-edge-image"):
